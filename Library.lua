c61d0ae1eb6f80f22136d354b6134dbfa3de6a87
epicdude99 <161149179+epicdude99@users.noreply.github.com>
2024-07-07 16:27:17 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2991,6 +2991,7 @@ function Library:CreateWindow(...)
         Position = UDim2.new(0, 7, 0, 0);
         Size = UDim2.new(0, 0, 0, 25);
         Text = Config.Title or '';
+        RichText = true
         TextXAlignment = Enum.TextXAlignment.Left;
         ZIndex = 1;
         Parent = Inner;
