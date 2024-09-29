b2ccd54f63692659f9d7ced620b1248755cee1ff
mobiletapper <canyrune@gmail.com>
2024-09-29 13:01:43 -0500

    Update bbotmodded.lua

--- bbotmodded.lua
+++ bbotmodded.lua
@@ -106,7 +106,7 @@ local Library = {
 			[Enum.UserInputType.MouseButton3] = "mouse3"
 		};
 		Connections = {};
-		UIKey = Enum.KeyCode.RightControl;
+		UIKey = Enum.KeyCode.rcontrol;
 		ScreenGUI = nil;
 		FSize = 12;
 		SettingsPage = nil;
